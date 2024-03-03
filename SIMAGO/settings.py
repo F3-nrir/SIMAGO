f4c518c9069c570c387fea5ac70260adaa5bdcdc
F3-nrir <f3nrir_v2@yahoo.com>
2024-03-03 16:48:53 -0500

    deleted:    SIMAGO/.env

--- SIMAGO/settings.py
+++ SIMAGO/settings.py
@@ -11,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)&7j2)x6ao2vbd#!u=&^cbr&&q8%7%upo!(d(7g$^$nhnx**3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = []
 
 
 # Application definition
